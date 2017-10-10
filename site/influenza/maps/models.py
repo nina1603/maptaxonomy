3e9138c265bc30a9fa4cc400f068086c2795001f
Sergey <fjdiod@gmail.com>
2017-10-11 00:59:00 +0300

    fix model

--- site/influenza/maps/models.py
+++ site/influenza/maps/models.py
@@ -4,16 +4,22 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Virus(models.Model):
-    
     ncbi_entry = models.URLField()
     date_extracted = models.DateTimeField()
+    name = models.CharField(max_length=500, default='Virus')
+    
+    def __str__(self):
+        return self.name
     
     
 class Location(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
-    position = models.CharField(max_length=500)
+    position = models.CharField(max_length=500, null=True)
     commentary = models.CharField(max_length=500)
     date_found = models.DateTimeField()
     virus = models.ForeignKey('Virus', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.position
 

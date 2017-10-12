7d27dbaaa6a8634554062fe43ab81058dbc2503c
Sergey <fjdiod@gmail.com>
2017-10-12 23:12:52 +0300

    change model

--- site/influenza/maps/admin.py
+++ site/influenza/maps/admin.py
@@ -3,7 +3,10 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Virus, Location
+from .models import Experiment, Author, Taxon, GenomePart, Strain
 
-admin.site.register(Virus)
-admin.site.register(Location)
+admin.site.register(Experiment)
+admin.site.register(Author)
+admin.site.register(Taxon)
+admin.site.register(GenomePart)
+admin.site.register(Strain)
--- site/influenza/maps/models.py
+++ site/influenza/maps/models.py
@@ -3,23 +3,45 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class Virus(models.Model):
+
+class Experiment(models.Model):
+    date_conducted = models.DateTimeField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    position = models.CharField(max_length=500, null=True)
     ncbi_entry = models.URLField()
-    date_extracted = models.DateTimeField()
-    name = models.CharField(max_length=500, default='Virus')
+    genbank_id = models.CharField(max_length=100, null=True)
+    
+    def __str__(self):
+        return self.position
+    
+    
+class Author(models.Model):
+    name = models.CharField(max_length=500, null=True)
+    experiment = models.ForeignKey('Experiment', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+class Strain(models.Model):
+    experiment = models.ForeignKey('Experiment', on_delete=models.CASCADE)
+    name = models.CharField(max_length=500)
     
     def __str__(self):
         return self.name
     
+
+class GenomePart(models.Model):
+    strain = models.ForeignKey('Strain', on_delete=models.CASCADE)
+    sequence = models.CharField(max_length=10000)
+    is_mobile = models.BooleanField()
+    taxon = models.ForeignKey('Taxon', on_delete=models.CASCADE)
     
-class Location(models.Model):
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-    position = models.CharField(max_length=500, null=True)
+
+class Taxon(models.Model):
+    name = models.CharField(max_length=500)
     commentary = models.CharField(max_length=500)
-    date_found = models.DateTimeField()
-    virus = models.ForeignKey('Virus', on_delete=models.CASCADE)
+    parent = models.ForeignKey('self',null=True,blank=True)
     
     def __str__(self):
-        return self.position
-
+        return self.name

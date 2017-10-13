964acb92dc41c3bffcc5de3592c9b8a9bf86a0cc
Sergey <fjdiod@gmail.com>
2017-10-13 14:01:04 +0300

    rename influenza to maptaxonomy

--- site/maptaxonomy/maptaxonomy/wsgi.py
+++ site/maptaxonomy/maptaxonomy/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for influenza project.
+WSGI config for maptaxonomy project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "influenza.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "maptaxonomy.settings")
 
 application = get_wsgi_application()

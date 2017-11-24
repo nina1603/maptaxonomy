7ad311454bbfc0e216dc01b60176c92da44c3c5f
Sergey Solovev <solovev@ws-finance.com>
2017-11-24 13:02:10 +0300

    add coord saving

--- site/maptaxonomy/loading/loading.py
+++ site/maptaxonomy/loading/loading.py
@@ -3,9 +3,28 @@ import gzip
 import datetime as dt
 from maps.models import Experiment, Strain
 import geocoder
+import re
 
 def load_file(url='ftp://ftp.ncbi.nih.gov/genomes/INFLUENZA/influenza_na.dat.gz', fname='tmp'):
     urlretrieve(url, fname)
+
+
+def extract_descr(row):
+    for column in row:
+        if column.lower().find('virus') != -1:
+            return column
+    return None
+
+
+def extract_info(descr):
+    if descr is None:
+        return None
+    info = re.findall('\(.*\)', descr)
+    if info:
+        return info[0]
+    else:
+        return None
+
 
 def process_date(date):
     count = date.count('/')
@@ -19,18 +38,35 @@ def process_date(date):
     except:
         return dt.date(1, 1, 1)
 
+
+def extract_coords(position, coords):
+    if position is not None:
+        if position in coords:
+            print('Using saved coords...')
+            lat, lng = coords[position]
+        else:
+            print('Using Google Maps api...')
+            try:
+                lat, lng = geocoder.google(position).latlng
+                coords.update({position: (lat, lng)})
+            except:
+                lat, lng = 0, 0
+    else:
+        lat, lng = 0, 0
+    return lat, lng
+
+
 def populate_db(fname='tmp'):
+    coords = {}
     with gzip.open(fname, 'r') as data:
         gen = map(lambda x: x.decode('utf8').strip('\n').split('\t'), data)
         gen = filter(lambda x: x[1]=='Human', gen)
         for line in gen:
             date = process_date(line[5])
-            try:
-                lat, lng = geocoder.google(line[4]).latlng
-            except:
-                lat, lng = 0, 0
-                
-            exp = Experiment(position=line[4], date_conducted=date, genbank_id=line[0], longitude=lat, latitude=lng)
+            info = extract_info(extract_descr(line))
+            position = info.split('/')[1] if info is not None else None
+            lat, lng = extract_coords(position, coords)
+            exp = Experiment(position=str(position), date_conducted=date, genbank_id=line[0], longitude=lat, latitude=lng)
             exp.save()
             strain = Strain(experiment=exp, name=line[7])
             strain.save()

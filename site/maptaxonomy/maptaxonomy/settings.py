894cb1e27b43ccd0c9d36544c28f2f25b82ac8cf
Mihail Vasilev <mickvav@gmail.com>
2017-12-01 21:16:48 +0300

    Added real hostname.

--- site/maptaxonomy/maptaxonomy/settings.py
+++ site/maptaxonomy/maptaxonomy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&i(q5j-cew526w5aa+%5*7nbt@gwtkyc*jydq2g%zw&pfiu)2t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ u'34.212.26.94', 'nina1603.github.io' ]
+ALLOWED_HOSTS = [ u'34.212.26.94', 'nina1603.github.io', 'maptaxonomy.ru', 'www.maptaxonomy.ru' ]
 
 
 # Application definition
@@ -139,6 +139,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 CORS_ORIGIN_WHITELIST = (
     'nina1603.github.io',
     'maptaxonomy.ru',
+    'www.maptaxonomy.ru',
     'localhost:8000',
     '127.0.0.1:9000'
 )

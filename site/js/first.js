6d5016343204f69078fb75aa40ead55addc90a8b
nina1603 <32387176+nina1603@users.noreply.github.com>
2018-03-27 15:10:21 +0300

    Update first.js

--- site/js/first.js
+++ site/js/first.js
@@ -9,39 +9,40 @@ a.style.display = "none";
 function loadTable()
       {
         var xhr = new XMLHttpRequest();
-	var places;
+		var places;
         xhr.open('GET', 'https://maptaxonomy.ru/api/v1/experiments/?format=json', true); ///maptaxonomy/sample_data/Table1.csv.json
         xhr.send();
         xhr.onreadystatechange = function()
         {
-          if ((xhr.readyState > 0) && (xhr.readyState < 4))
-              a.style.display = "block";
-			if (xhr.readyState == 4)
+			if ((xhr.readyState > 0) && (xhr.readyState < 4))
 			{
-				a.style.display = "none";
-          if (xhr.status != 200) 
-          {
-            // обработать ошибку
-            alert( xhr.status + ': ' + xhr.statusText );
+				a.style.display = "block";
+				if (xhr.readyState == 4)
+				{
+					a.style.display = "none";
+					if (xhr.status != 200) 
+					{
+						// обработать ошибку
+						alert( xhr.status + ': ' + xhr.statusText );
+					}
+				else
+				{
+					try
+					{
+						var data = JSON.parse(xhr.responseText);
+						places = data.results;
+					}
+					catch (e)
+					{
+					alert( "Некорректный ответ: " + e.message);
+					console.log(xhr.responseText);            
+					}
+				}
+				showTable(places);
+				}
           }
-          else
-          {
-            try
-            {
-              var data = JSON.parse(xhr.responseText);
-              places = data.results;
-		
-            }
-            catch (e)
-            {
-              alert( "Некорректный ответ: " + e.message);
-		console.log(xhr.responseText);            
-	    }
-	  }
-            showTable(places);
-		}
-          }
-      }	
+      }
+     }
 	 
 	 var columnHandlers = {
 				'genbank': function(x) {return 'Genbank: ' + 
@@ -107,30 +108,8 @@ function loadTable()
 		infowindow[place] = new google.maps.InfoWindow({
           content: contentString[place]
        });
-        };
+     };
 		marker[place].infowindow = infowindow[place];
-	     marker[place].addListener('click', function() {
-          return this.infowindow.open(map, this);
+	     	marker[place].addListener('click', function() {
+          	return this.infowindow.open(map, this); }
 	}
-	} 
-	
-	
-	/* if (header == 'Name')
-		{
-			var local = places[place][header].split('/');
-			arr[place] = {};
-			arr[place].city = local[2];
-		}	
-		if (header == 'Country')
-		{
-				arr[place].country = places[place][header];
-		}
-		if (header == 'latitude')
-				var lat = places[place][header];
-		if (header == 'longitude')
-				var lng = places[place][header];
-		var loc = {lat: lat, lng: lng};
-		if (header == 'position')
-			var pos = places[place][header]
-		if (header == 'genbank_id')
-			var id = places[place][header];*/

ad9e12abd20da834c8993bf8fa9baab0d16627c6
nina1603 <32387176+nina1603@users.noreply.github.com>
2018-04-01 17:47:06 +0300

    Create init.js

---
+++ site/js/init.js
@@ -0,0 +1,54 @@
+var map;
+	function initMap() {
+        	var location = {lat: 55.700, lng: 37.600};
+        	var uluru = {lat: -25.363, lng: 131.044};
+        	map = new google.maps.Map(document.getElementById('map'), {
+        	  zoom: 4,
+       		  center: location
+        	});
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map,
+          draggable: true,
+          animation: google.maps.Animation.DROP,
+          title: 'Marker'
+        });
+        var marker2 = new google.maps.Marker({ 
+          position: location,
+          map: map,
+          draggable: true,
+          title: 'Second Marker' 
+        });
+        var contentString = 'Hello! I am the Marker. Move me :)';
+        var infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+        marker.addListener('click', function() {
+          infowindow.open(map, marker);
+       });
+	marker2.addListener('click', function() {
+          infowindow.open(map, marker2);
+       });      
+	google.maps.event.addListener(marker, 'position_changed', update);
+  	google.maps.event.addListener(marker2, 'position_changed', update);      
+	      
+        var flightPlanCoordinates = [
+       marker.getPosition(), 
+       marker2.getPosition()
+        ];
+        var flightPath = new google.maps.Polyline( {
+        path: flightPlanCoordinates, 
+        geodesic: true,
+	draggable: true,
+        strokeColor: '#FF0000',
+      	strokeOpacity: 1.0,
+      	strokeWeight: 2
+    } );
+
+  	flightPath.setMap(map);
+	update();
+function update() {
+  var path = [marker.getPosition(), marker2.getPosition()];
+  flightPath.setPath(path);
+}
+      }

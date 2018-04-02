97e6bf266d2211fdae4b74a39993c745eb961b3c
nina1603 <32387176+nina1603@users.noreply.github.com>
2018-04-02 15:08:50 +0300

    Update sheetDesign.js

--- site/js/sheetDesign.js
+++ site/js/sheetDesign.js
@@ -1,21 +1,21 @@
 // Client ID and API key from the Developer Console
-      var CLIENT_ID = '858139403726-0ru9uicbkoo5o3i98idspa9onocbhi5n.apps.googleusercontent.com';
-      var API_KEY = 'AIzaSyBqxSOO8-ABdeDtEapbKXWC_7j7g57HC18';
-      // Array of API discovery doc URLs for APIs used by the quickstart
+var CLIENT_ID = '858139403726-0ru9uicbkoo5o3i98idspa9onocbhi5n.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyBqxSOO8-ABdeDtEapbKXWC_7j7g57HC18';
+// Array of API discovery doc URLs for APIs used by the quickstart
       var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
       // Authorization scopes required by the API; multiple scopes can be
       // included, separated by spaces.
       var SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
       var authorizeButton = document.getElementById('authorize-button');
       var signoutButton = document.getElementById('signout-button');
-      /**
-       *  On load, called to load the auth2 library and API client library.
+
+      /* On load, called to load the auth2 library and API client library.
        */
       function handleClientLoad() {
         gapi.load('client:auth2', initClient);
       }
-      /**
-       *  Initializes the API client library and sets up sign-in state
+
+      /* Initializes the API client library and sets up sign-in state
        *  listeners.
        */
       function initClient() {
@@ -33,8 +33,8 @@
           signoutButton.onclick = handleSignoutClick;
         });
       }
-      /**
-       *  Called when the signed in status changes, to update the UI
+
+      /* Called when the signed in status changes, to update the UI
        *  appropriately. After a sign-in, the API is called.
        */
       function updateSigninStatus(isSignedIn) {
@@ -47,20 +47,20 @@
           signoutButton.style.display = 'none';
         }
       }
-      /**
-       *  Sign in the user upon button click.
+
+      /*  Sign in the user upon button click.
        */
       function handleAuthClick(event) {
         gapi.auth2.getAuthInstance().signIn();
       }
-      /**
-       *  Sign out the user upon button click.
+
+      /*  Sign out the user upon button click.
        */
       function handleSignoutClick(event) {
         gapi.auth2.getAuthInstance().signOut();
       }
-      /**
-       * Append a pre element to the body containing the given message
+
+      /* Append a pre element to the body containing the given message
        * as its text node. Used to display the results of the API call.
        *
        * @param {string} message Text to be placed in pre element.
@@ -70,11 +70,11 @@
         var textContent = document.createTextNode(message + '\n'); 
         pre.appendChild(textContent);
       }
-      /**
-       * Print the names and majors of students in a sample spreadsheet:
+
+      /* Print the names and majors of students in a sample spreadsheet:
        * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
        */
-      function listMajors() {
+      /*function listMajors() {
         gapi.client.sheets.spreadsheets.values.get({
           spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
           range: 'Class Data!A2:E',
@@ -93,4 +93,4 @@
         }, function(response) {
           appendPre('Error: ' + response.result.error.message);
         });
-      }
+      }*/

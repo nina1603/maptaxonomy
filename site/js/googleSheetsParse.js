88710941e6756ea1c2b314390c3e07cc4ad14fc3
nina1603 <32387176+nina1603@users.noreply.github.com>
2018-04-03 18:41:27 +0300

    Update googleSheetsParse.js

--- site/js/googleSheetsParse.js
+++ site/js/googleSheetsParse.js
@@ -91,7 +91,7 @@ function listPlaces(address) {
             var mainStr = range.values[0];
             for (i = 1; i < range.values.length; i++) {
                 var row = range.values[i];
-                appendPre(row[parser(mainStr, 'genbank_id')] + ', ' + row[parser(mainStr, 'name')]);
+                appendPre(row[parser(mainStr, 'genbank_id')] + ', ' + row[parser(mainStr, 'name')] + row[parser(mainStr, 'position')]);
                 marker[i] = new google.maps.Marker({
                     position: {
                         lat: Number.parseInt(row[parser(mainStr, 'latitude')]),
@@ -123,5 +123,5 @@ function parser(str, name) {
         if (str[i] == name)
             return i;
     }
-    return 0;
+    return -1;
 }

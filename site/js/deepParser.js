41c7e5a40e18433250c88e1bf319633a3f4ca910
nina1603 <32387176+nina1603@users.noreply.github.com>
2018-05-09 00:24:03 +0300

    Update deepParser.js

--- site/js/deepParser.js
+++ site/js/deepParser.js
@@ -1,5 +1,7 @@
 var down = document.getElementById("downTriangle");
 var up = document.getElementById("upTriangle");
+var wind = document.getElementById("leftWindow");
+var hidden = document.getElementById("hidden");
 
 down.onclick = openExtraField;
 up.onclick = closeExtraField;
@@ -8,10 +10,12 @@ function openExtraField(event)
 {
   up.style.display = 'block';
   down.style.display = 'none';
+  hidden.style.display = 'block';
   }
   
   function closeExtraField(event)
 {
   down.style.display = 'block';
   up.style.display = 'none';
+  hidden.style.display = 'none';
   }
